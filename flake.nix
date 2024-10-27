1c0f400281acce273c5dbd9b1b036166e70b9386
MonsterDruide1 <5958456@gmail.com>
2024-10-27 10:23:48 +0100

    dev: Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    perSystem = { pkgs, ... }: {
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = [
+          pkgs.nodePackages.npm
+        ];
+      };
+
+    };
+  };
+}
